4e0052be24094a73654230aece7df21682e4b8c3
Dan Burnley <dan@danops.uk>
2022-02-14 16:55:51 +0000

    Deployment for cola integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,17 +4,17 @@ require "json"
 
 class HomeController < ApplicationController
   def index
-    @user = current_user
-    payload = { user: current_user.email }
-    @token = JWT.encode payload, nil, 'none'
+#     @user = current_user
+     payload = { user: 'example' }
+     @token = JWT.encode payload, nil, 'none'
 
-    forms_response = HTTParty.get(
-      "#{ENV['FORMS_API_URL']}published",
-      headers: {
-        "X_API_KEY" => @token
-      }
-    )
+     forms_response = HTTParty.get(
+       "#{ENV['FORMS_API_URL']}published",
+       headers: {
+         "X_API_KEY" => @token
+       }
+     )
 
-    @forms = JSON.parse(forms_response.body)
+     @forms = JSON.parse(forms_response.body)
   end
 end
